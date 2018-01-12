a2570113c7f3e62cdd27ed31db5067d0c62f1dc1
Kyu Hyung Choi <choigww@gmail.com>
2018-01-12 23:41:28 +0900

    'update'

---
+++ udemy-beginning-modern-c++/first program/first program/main.cpp
@@ -0,0 +1,18 @@
+// This is a single line comment
+/*
+This is a
+multi line comment
+*/
+#include <iostream>
+
+// Execution of program starts here
+int main() {
+	// Open the std namespace
+	using namespace std;
+
+	// Print the string on the screen
+    cout << "Hello " << "C++" << endl;
+	
+	//Return the exit code
+	return 0;
+}
